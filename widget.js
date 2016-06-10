7a65437f71b9b5371d9fa1ea6a86ace15a00c610
Peter Schmalfeldt <peter@manifestinteractive.com>
2016-06-09 22:58:37 -0400

    Updating CSS for Bill Listing

--- widget.js
+++ widget.js
@@ -9,7 +9,7 @@
   var elementName = 'campaign-zero-widget';
   var loadedCSS = false;
   var loadedJS = false;
-  var version = '1.0.4';
+  var version = '1.0.5';
 
   /** Get reference to self (scriptTag) */
   var allScripts = document.getElementsByTagName('script');
