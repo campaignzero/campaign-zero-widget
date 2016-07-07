a11226aa38fa2f9521b010af4dabfabc1c73b79b
samnicky <sam@thisisthemovement.org>
2016-07-07 08:14:48 -0700

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,6 @@
+<?php
+
+// Get `API_KEY` Here:  https://sunlightfoundation.com/api/accounts/register/ …
+define('API_KEY', 'a31a13474e2d401caa2352a555b62d00');
+define('API_URL_LEGISLATORS', 'http://openstates.org/api/v1/legislators/geo/ …');
+define('API_URL_BILLS', 'http://openstates.org/api/v1/bills/ ');
