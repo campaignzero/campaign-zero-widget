6c71feb57609eb670b07ea36daf0900ac86f9a8c
Peter Schmalfeldt <peter@manifestinteractive.com>
2016-07-07 18:24:11 -0400

    updating config to add new params.  Make sure to update your local config.php file with the new params.

--- app/config.dist.php
+++ app/config.dist.php
@@ -4,3 +4,4 @@
 define('API_KEY', 'REPLACE_ME');
 define('API_URL_LEGISLATORS', 'http://openstates.org/api/v1/legislators/geo/');
 define('API_URL_BILLS', 'http://openstates.org/api/v1/bills/');
+define('CACHE_PATH', '../cache/');
