c2a9eb65ba86b0fc1ba284fcd424c01a1827e46e
Peter Schmalfeldt <manifestinteractive@users.noreply.github.com>
2016-07-07 17:17:32 -0400

    Delete config.php

--- app/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Get `API_KEY` Here:  https://sunlightfoundation.com/api/accounts/register/ …
-define('API_KEY', 'a31a13474e2d401caa2352a555b62d00');
-define('API_URL_LEGISLATORS', 'http://openstates.org/api/v1/legislators/geo/');
-define('API_URL_BILLS', 'http://openstates.org/api/v1/bills/');
-define('CACHE_PATH', '../cache/');
