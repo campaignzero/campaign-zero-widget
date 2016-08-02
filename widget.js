db25b643d3d7f59886ac0991e0182c0676114349
Peter Schmalfeldt <peter@manifestinteractive.com>
2016-08-02 08:10:53 -0400

    Updating version number to bust cache on sites already running widget

--- widget.js
+++ widget.js
@@ -9,7 +9,7 @@
   var elementName = 'campaign-zero-widget';
   var loadedCSS = false;
   var loadedJS = false;
-  var version = '1.0.8';
+  var version = '1.0.9';
 
   /** Get reference to self (scriptTag) */
   var allScripts = document.getElementsByTagName('script');
