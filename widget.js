e8bc3ab75cbfbeadfa1600d5a37820ecf572ba42
Peter Schmalfeldt <peter@manifestinteractive.com>
2017-03-24 03:07:28 -0400

    Update Version Number

--- widget.js
+++ widget.js
@@ -9,7 +9,7 @@
   var elementName = 'campaign-zero-widget';
   var loadedCSS = false;
   var loadedJS = false;
-  var version = '1.3.2';
+  var version = '1.3.3';
 
   /** Get reference to self (scriptTag) */
   var allScripts = document.getElementsByTagName('script');
